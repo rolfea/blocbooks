cc30449c3170aa759a00ad056327e0055e99da08
Alex Rolfe <rolfepercussion@gmail.com>
2016-11-03 11:25:00 -0500

    Adds create message for testing

--- bloc-books/app/controllers/books_controller.rb
+++ bloc-books/app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < BlocWorks::Controller
   def welcome
     render :welcome, book: "Eloquent Ruby"
   end
+
+  def create
+    puts "create"
+  end
+
+  def read
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- bloc-books/config/application.rb
+++ bloc-books/config/application.rb
@@ -1,6 +1,6 @@
 require "bloc_works"
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers"
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
 module BlocBooks
   class Application < BlocWorks::Application
